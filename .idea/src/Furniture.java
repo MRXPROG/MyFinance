f88a9f263797d6deb4bad5625905ee2c91e04754
root <evgenmaslov90@gmail.com>
2022-08-13 14:15:14 +0200

    Нормально

--- .idea/src/Furniture.java
+++ .idea/src/Furniture.java
@@ -1,2 +1,3 @@
-public class Furniture {
+public class Furniture  {
+
 }
---
+++ .idea/src/Things.java
@@ -0,0 +1,8 @@
+public abstract class Things {
+    String name;
+    double cost;
+    public Things(String name, double cost){
+        this.name = name;
+        this.cost = cost;
+    }
+}
